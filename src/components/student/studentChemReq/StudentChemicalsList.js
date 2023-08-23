936ab522426e8a29d1e972313a603f0cc8b92f9b
Ball Panuwat <79345283+ballpanuwat25@users.noreply.github.com>
2023-08-23 18:34:56 +0700

    Source Code Updated

--- src/components/student/studentChemReq/StudentChemicalsList.js
+++ src/components/student/studentChemReq/StudentChemicalsList.js
@@ -13,7 +13,106 @@ function StudentChemicalsList() {
         Chem_Id: "",
     });
 
-    const [selectedChemicalsId, setSelectedChemicalsId] = useState(null);
+    const [selectedChemicalsId, setSelectedChemicalsId] = useState({});
+
+    const [chemicalsReq, setChemicalsReq] = useState([]);
+    const [mostRequestedChemIds, setMostRequestedChemIds] = useState([]);
+
+    const [mostRequestedEquipIds, setMostRequestedEquipIds] = useState([]);
+
+    useEffect(() => {
+        getChemicalsRequest();
+        getEquipmentRequest();
+    }, []);
+
+    const getChemicalsRequest = async () => {
+        try {
+            const response = await axios.get("http://localhost:3001/chemicals-request-list");
+            setChemicalsReq(response.data);
+            findMostRequestedChemIds(response.data, 3);
+        } catch (error) {
+            console.error("Error fetching chemicals request:", error);
+        }
+    };
+
+    const findMostRequestedChemIds = (data, n) => {
+        const chemIdCounts = {};
+
+        data.forEach(request => {
+            const chemId = request.Chem_Id;
+            chemIdCounts[chemId] = (chemIdCounts[chemId] || 0) + 1;
+        });
+
+        const sortedChemIds = Object.keys(chemIdCounts).sort((a, b) => chemIdCounts[b] - chemIdCounts[a]);
+
+        const mostRequestedIds = sortedChemIds.slice(0, n);
+
+        setMostRequestedChemIds(mostRequestedIds);
+    };
+
+    const addChemToCartFromOffcanvas = (chemId) => {
+        // Get the existing cart data from localStorage (if any)
+        const cartData = JSON.parse(localStorage.getItem('chemicalsCart')) || [];
+
+        // Check if the selected chemicals are already in the cart
+        const existingChemical = cartData.find(item => item.Student_Id === studentId && item.Chem_Id === chemId);
+
+        if (existingChemical) {
+            alert('This chemical is already in your cart');
+            return;
+        }
+
+        cartData.push({
+            Student_Id: studentId,
+            Chem_Id: chemId,
+        });
+
+        setSelectedChemicalsId({ Chem_Id: chemId });
+        localStorage.setItem('chemicalsCart', JSON.stringify(cartData));
+    };
+
+    const getEquipmentRequest = async () => {
+        try {
+            const response = await axios.get("http://localhost:3001/equipment-request-list");
+            findMostRequestedEquipIds(response.data, 3);
+        } catch (error) {
+            console.error("Error fetching equipment request:", error);
+        }
+    };
+
+    const findMostRequestedEquipIds = (data, n) => {
+        const equipIdCounts = {};
+
+        data.forEach(request => {
+            const equipId = request.Equipment_Id;
+            equipIdCounts[equipId] = (equipIdCounts[equipId] || 0) + 1;
+        });
+
+        const sortedEquipIds = Object.keys(equipIdCounts).sort((a, b) => equipIdCounts[b] - equipIdCounts[a]);
+
+        const mostRequestedIds = sortedEquipIds.slice(0, n);
+
+        setMostRequestedEquipIds(mostRequestedIds);
+    };
+
+    const addEquipToCartFromOffcanvas = (equipId) => {
+        // Get the existing cart data from localStorage (if any)
+        const cartData = JSON.parse(localStorage.getItem('equipmentCart')) || [];
+
+        // Check if the selected equipment are already in the cart
+        const existingEquipment = cartData.find(item => item.Student_Id === studentId && item.Equipment_Id === equipId);
+
+        if (existingEquipment) {
+            alert('This equipment is already in your cart');
+        } else {
+            cartData.push({
+                Student_Id: studentId,
+                Equipment_Id: equipId,
+            });
+        }
+
+        localStorage.setItem('equipmentCart', JSON.stringify(cartData));
+    };
 
     useEffect(() => {
         axios.get("http://localhost:3001/student").then((response) => {
@@ -51,13 +150,12 @@ function StudentChemicalsList() {
         if (existingChemical) {
             alert('This chemical is already in your cart');
         } else {
-            // If the selected chemicals are not in the cart, add them as a new item
             cartData.push({
                 Student_Id: studentId,
                 Chem_Id,
             });
         }
-
+        setSelectedChemicalsId({ Chem_Id: Chem_Id });
         localStorage.setItem('chemicalsCart', JSON.stringify(cartData));
     };
 
@@ -106,22 +204,85 @@ function StudentChemicalsList() {
                     </tr>
                 </thead>
                 <tbody>
-                    {filteredChemicals.map((chemicalsDetail, index) => (
-                        <tr key={index}>
-                            <td> {index + 1} </td>
-                            <td> {chemicalsDetail.Chem_Name} </td>
-                            <td> {chemicalsDetail.Chem_CAS} </td>
-                            <td> {chemicalsDetail.Chem_UN} </td>
-                            <td> {chemicalsDetail.Chem_Type} </td>
-                            <td> {chemicalsDetail.Chem_Grade} </td>
-                            <td> {chemicalsDetail.Chem_State} </td>
-                            <td>
-                                <div className="d-grid gap-2 d-sm-flex">
-                                    <button className="btn btn-primary" onClick={() => addToCart(chemicalsDetail.Chem_Id)}>add to cart</button>
-                                </div>
-                            </td>
-                        </tr>
-                    ))}
+                    {filteredChemicals.map((chemicalsDetail, index) => {
+                        // Filter the chemicalsReq list to find unique Chem_Id types
+                        const uniqueChemIdTypes = [...new Set(chemicalsReq.map(request => request.Chem_Id))];
+
+                        // Check if the number of unique Chem_Id types is less than 3
+                        const isOffCanvasEnabled = uniqueChemIdTypes.length >= 3;
+
+                        return (
+                            <tr key={index}>
+                                <td> {index + 1} </td>
+                                <td> {chemicalsDetail.Chem_Name} </td>
+                                <td> {chemicalsDetail.Chem_CAS} </td>
+                                <td> {chemicalsDetail.Chem_UN} </td>
+                                <td> {chemicalsDetail.Chem_Type} </td>
+                                <td> {chemicalsDetail.Chem_Grade} </td>
+                                <td> {chemicalsDetail.Chem_State} </td>
+                                <td>
+                                    <div className="d-grid gap-2 d-sm-flex">
+                                        <button
+                                            className="btn btn-primary"
+                                            onClick={() => addToCart(chemicalsDetail.Chem_Id)}
+                                            data-bs-toggle={isOffCanvasEnabled ? "offcanvas" : ""}
+                                            data-bs-target={isOffCanvasEnabled ? "#offcanvasWithBackdrop1" : ""}
+                                            aria-controls="offcanvasWithBackdrop"
+                                        >
+                                            add to cart
+                                        </button>
+
+                                        {isOffCanvasEnabled && (
+                                            <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasWithBackdrop1" aria-labelledby="offcanvasWithBackdropLabel">
+                                                <div className="offcanvas-header">
+                                                    <h3 className="offcanvas-title" id="offcanvasWithBackdropLabel">{selectedChemicalsId.Chem_Id} is added to cart</h3>
+                                                    <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                                </div> <hr />
+                                                <div className="offcanvas-body">
+                                                    {selectedChemicalsId && (
+                                                        <div>
+                                                            <h5> Chemicals Suggestions </h5>
+                                                            <ul>
+                                                                {mostRequestedChemIds.map((chemId, index) => (
+                                                                    selectedChemicalsId.Chem_Id !== chemId && (
+                                                                        <li key={index}>
+                                                                            {chemId}
+                                                                            <button
+                                                                                className="btn btn-primary ms-2 mb-2"
+                                                                                onClick={() => addChemToCartFromOffcanvas(chemId)}
+                                                                            >
+                                                                                add to cart
+                                                                            </button>
+                                                                        </li>
+                                                                    )
+                                                                ))}
+                                                            </ul>
+
+                                                        </div>
+                                                    )}
+                                                    <hr />
+                                                    <h5> Equipment Suggestions </h5>
+                                                    <ul>
+                                                        {mostRequestedEquipIds.map((equipId, index) => (
+                                                            <li key={index}>
+                                                                {equipId}
+                                                                <button
+                                                                    className="btn btn-primary ms-2 mb-2"
+                                                                    onClick={() => addEquipToCartFromOffcanvas(equipId)}
+                                                                >
+                                                                    add to cart
+                                                                </button>
+                                                            </li>
+                                                        ))}
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        )}
+                                    </div>
+                                </td>
+                            </tr>
+                        );
+                    })}
                 </tbody>
             </table>
         </div>
--- src/components/student/studentEquipmentReq/StudentEquipmentList.js
+++ src/components/student/studentEquipmentReq/StudentEquipmentList.js
@@ -11,10 +11,110 @@ function StudentEquipmentList() {
     const [equipmentRequest, setEquipmentRequest] = useState({
         Student_Id: "",
         Equipment_Id: "",
-        Requested_Quantity: "",
+        Requested_Quantity: 1,
     });
 
-    const [selectedEquipmentId, setSelectedEquipmentId] = useState(null);
+    const [selectedEquipmentId, setSelectedEquipmentId] = useState({});
+
+    const [equipmentReq, setEquipmentReq] = useState([]);
+    const [mostRequestedEquipIds, setMostRequestedEquipIds] = useState([]);
+
+    const [mostRequestedChemIds, setMostRequestedChemIds] = useState([]);
+
+    useEffect(() => {
+        getEquipmentRequest();
+        getChemicalsRequest();
+    }, []);
+
+    const getEquipmentRequest = async () => {
+        try {
+            const response = await axios.get("http://localhost:3001/equipment-request-list");
+            setEquipmentReq(response.data);
+            findMostRequestedEquipIds(response.data, 3);
+        } catch (error) {
+            console.error("Error fetching equipment request:", error);
+        }
+    };
+
+    const findMostRequestedEquipIds = (data, n) => {
+        const equipIdCounts = {};
+
+        data.forEach(request => {
+            const equipId = request.Equipment_Id;
+            equipIdCounts[equipId] = (equipIdCounts[equipId] || 0) + 1;
+        });
+
+        const sortedEquipIds = Object.keys(equipIdCounts).sort((a, b) => equipIdCounts[b] - equipIdCounts[a]);
+
+        const mostRequestedIds = sortedEquipIds.slice(0, n);
+
+        setMostRequestedEquipIds(mostRequestedIds);
+    };
+
+    const addEquipToCartFromOffcanvas = (equipId) => {
+        // Get the existing cart data from localStorage (if any)
+        const cartData = JSON.parse(localStorage.getItem('equipmentCart')) || [];
+
+        // Check if the selected equipment are already in the cart
+        const existingEquipment = cartData.find(item => item.Student_Id === studentId && item.Equipment_Id === equipId);
+
+        if (existingEquipment) {
+            alert('This equipment is already in your cart');
+            return;
+        } 
+        
+        cartData.push({
+            Student_Id: studentId,
+            Equipment_Id: equipId,
+        });
+
+        setSelectedEquipmentId({ Equipment_Id: equipId });
+        localStorage.setItem('equipmentCart', JSON.stringify(cartData));
+    };
+
+    const getChemicalsRequest = async () => {
+        try {
+            const response = await axios.get("http://localhost:3001/chemicals-request-list");
+            findMostRequestedChemIds(response.data, 3);
+        } catch (error) {
+            console.error("Error fetching chemicals request:", error);
+        }
+    };
+
+    const findMostRequestedChemIds = (data, n) => {
+        const chemIdCounts = {};
+
+        data.forEach(request => {
+            const chemId = request.Chem_Id;
+            chemIdCounts[chemId] = (chemIdCounts[chemId] || 0) + 1;
+        });
+
+        const sortedChemIds = Object.keys(chemIdCounts).sort((a, b) => chemIdCounts[b] - chemIdCounts[a]);
+
+        const mostRequestedIds = sortedChemIds.slice(0, n);
+
+        setMostRequestedChemIds(mostRequestedIds);
+    };
+
+    const addChemToCartFromOffcanvas = (chemId) => {
+        // Get the existing cart data from localStorage (if any)
+        const cartData = JSON.parse(localStorage.getItem('chemicalsCart')) || [];
+
+        // Check if the selected chemicals are already in the cart
+        const existingChemical = cartData.find(item => item.Student_Id === studentId && item.Chem_Id === chemId);
+
+        if (existingChemical) {
+            alert('This chemical is already in your cart');
+            return;
+        } else {
+            cartData.push({
+                Student_Id: studentId,
+                Chem_Id: chemId,
+            });
+
+            localStorage.setItem('chemicalsCart', JSON.stringify(cartData));
+        };
+    };
 
     useEffect(() => {
         axios.get("http://localhost:3001/student").then((response) => {
@@ -60,11 +160,10 @@ function StudentEquipmentList() {
                 Requested_Quantity: equipmentRequest.Requested_Quantity,
             });
         }
-
-        // Save the cart data to localStorage
+        setSelectedEquipmentId({ Equipment_Id: Equipment_Id });
         localStorage.setItem('equipmentCart', JSON.stringify(cartData));
     }
-    
+
     const handleSearch = (e) => {
         setSearchQuery(e.target.value);
         filterEquipment(e.target.value);
@@ -106,18 +205,79 @@ function StudentEquipmentList() {
                     </tr>
                 </thead>
                 <tbody>
-                    {filteredEquipment.map((equipment, index) => (
-                        <tr key={index}>
-                            <td> {index + 1} </td>
-                            <td> {equipment.Equipment_Name} </td>
-                            <td> {equipment.Equipment_Category_Id} </td>
-                            <td>
-                                <div className="d-grid gap-2 d-sm-flex">
-                                    <button className="btn btn-primary" onClick={() => addToCart(equipment.Equipment_Id)}>add to cart</button>
-                                </div>
-                            </td>
-                        </tr>
-                    ))}
+                    {filteredEquipment.map((equipment, index) => {
+                        const uniqueEquipIdTypes = [...new Set(equipmentReq.map(request => request.Equipment_Id))];
+
+                        const isOffCanvasEnabled = uniqueEquipIdTypes.length >= 2;
+
+                        return (
+                            <tr key={index}>
+                                <td> {index + 1} </td>
+                                <td> {equipment.Equipment_Name} </td>
+                                <td> {equipment.Equipment_Category_Id} </td>
+                                <td>
+                                    <div className="d-grid gap-2 d-sm-flex">
+                                        <button
+                                            className="btn btn-primary"
+                                            onClick={() => addToCart(equipment.Equipment_Id)}
+                                            data-bs-toggle={isOffCanvasEnabled ? "offcanvas" : ""}
+                                            data-bs-target={isOffCanvasEnabled ? "#offcanvasWithBackdrop2" : ""}
+                                            aria-controls="offcanvasWithBackdrop"
+                                        >
+                                            add to cart
+                                        </button>
+
+                                        {isOffCanvasEnabled && (
+                                            <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasWithBackdrop2" aria-labelledby="offcanvasWithBackdropLabel">
+                                                <div className="offcanvas-header">
+                                                    <h3 className="offcanvas-title" id="offcanvasWithBackdropLabel">{selectedEquipmentId.Equipment_Id} is added to cart</h3>
+                                                    <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                                </div> <hr />
+                                                <div className="offcanvas-body">
+                                                    {selectedEquipmentId && (
+                                                        <div>
+                                                            <h5> Equipment Suggestions </h5>
+                                                            <ul>
+                                                                {mostRequestedEquipIds.map((equipId, index) => (
+                                                                    selectedEquipmentId.Equipment_Id !== equipId && (
+                                                                        <li key={index}>
+                                                                            {equipId}
+                                                                            <button
+                                                                                className="btn btn-primary ms-2 mb-2"
+                                                                                onClick={() => addEquipToCartFromOffcanvas(equipId)}
+                                                                            >
+                                                                                add to cart
+                                                                            </button>
+                                                                        </li>
+                                                                    )
+                                                                ))}
+                                                            </ul>
+
+                                                        </div>
+                                                    )}
+                                                    <hr />
+                                                    <h5> Chemicals Suggestions </h5>
+                                                    <ul>
+                                                        {mostRequestedChemIds.map((chemId, index) => (
+                                                            <li key={index}>
+                                                                {chemId}
+                                                                <button
+                                                                    className="btn btn-primary ms-2 mb-2"
+                                                                    onClick={() => addChemToCartFromOffcanvas(chemId)}
+                                                                >
+                                                                    add to cart
+                                                                </button>
+                                                            </li>
+                                                        ))}
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        )}
+                                    </div>
+                                </td>
+                            </tr>
+                        );
+                    })}
                 </tbody>
             </table>
         </div>
